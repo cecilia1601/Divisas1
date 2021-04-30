daa23c27f3d4cbeae53d0f5263a543f315f1b325
Cecilia1601 <cexica09@gmail.com>
2021-04-29 19:33:38 -0500

    Update divisas.c

--- divisas.c
+++ divisas.c
@@ -1,59 +1,48 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 /*
 Autor: Cecilia L.
 Description: covertidor de pesos a (Libras, Dolares Americanos, Euros)
 */
-int main()¨{
-//Declarar e inicializar 2 variables o mas (l, d,  e)
-float p = 1, d = 20, resultadoMultiplicacion = 20
-int operacion, opcion;
+int main(){
+//Declarar e inicializar 2 variables o mas (l, d, e)
+int dolar = 20, euro = 24, libra = 28, p = 1, opcion, pesos;
 printf("-----------------------------");
 printf("que quieres hacer?");
-printf("1.- Realizar operaciones\n 2.- Salir ");
-if 1.- printf("A que moneda quieres cambiar los pesos?: ");
-printf("1.-Dolares \n 2.- Euros \n 3.- Libras \n 4.- Salir\n ")
-scanf("%d", &opcion)
-int d = 20 ;
-int e = 24 ;
-int l = 28 ;
-int p = 1 ;
-int operacion, guardar suma;
-//ingresar los datos (p, l, d, e)
-scanf ("% d", &p);
-scanf("%d" , &l)
-scanf("%d" , &d)
-scanf("%d" , &e)
-//elegir el tipo de moneda (l, d ,e)
+printf("1. Realizar operaciones \n 2. Salir ");
+scanf("%d", &opcion);
+if (opcion != 1)
+{
+    printf("salio del programa \n");
+    exit (0);
+}
+else{
 printf("A que moneda quieres cambiar los pesos?: ");
-printf("1.-Dolares \n 2.- Euros \n 3.- Libras \n 4.- Salir\n ")
-// ingresa el monto
-printf("ingresa el monto: ");
-res= p * l 
-res= p * d
-res= p * e
-scanf("%d", operacion)
+//elegir el tipo de moneda (l, d ,e)
+printf("1.Dolares \n 2.Euros \n 3.Libras \n 4.Salir \n ");
+scanf("%d", &opcion);
+printf("ingresa el monto:");
+scanf("%d", &pesos);
 //imprimir el resultado
-    switch (operacion){
+    switch ( pesos ){
         //declarations
         // . . . 
-    case 1 :
-        guardar operacion = p * d
-        printf ("\tEl resultado de %d * %d = %d\n", d, e, l, p * d);
-        break;
+    case 1:
+    printf( "nada" pesos, (pesos / dolar))
+    break;
 
-    case 2 :
-        guardar operacion = p * e
-         printf ("\tEl resultado de %d * %d = %d\n", d, e, l, p * e);
+    case 2:
+        printf("\n", pesos, (pesos / euro));
         break;
 
     case 3:
-        guardar operacion = p * l
-         printf ("\tEl resultado de %d * %d = %d\n", d, e, l, p * l);
+        printf("\n", pesos, (pesos / libra));
         break;
     
     default:
-        printf("salir\n");
+        printf("salir \n");
         break;
     }
-    
+    return (0);
+}
+}
