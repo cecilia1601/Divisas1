1cce8d93eda1dfb419c8e675f4a65d2209f8de87
Dannas Cornell <dannas.cornell@gmail.com>
2021-06-21 17:59:59 -0500

    Update divisas.c

--- divisas.c
+++ divisas.c
@@ -6,9 +6,10 @@ Description: covertidor de pesos a (Libras, Dolares Americanos, Euros)
 */
 int main(){
 //Declarar e inicializar 2 variables o mas (l, d, e)
-int dolar = 20, euro = 24, libra = 28, p = 1, opcion, pesos;
+int dolar = 20, euro = 24, libra = 28, p = 1, opcion;
+float pesos;
 printf("-----------------------------");
-printf("que quieres hacer?");
+printf("que quieres hacer?\n");
 printf("1. Realizar operaciones \n 2. Salir ");
 scanf("%d", &opcion);
 if (opcion != 1)
@@ -17,26 +18,26 @@ if (opcion != 1)
     exit (0);
 }
 else{
-printf("A que moneda quieres cambiar los pesos?: ");
+printf("A que moneda quieres cambiar los pesos?:\n ");
 //elegir el tipo de moneda (l, d ,e)
 printf("1.Dolares \n 2.Euros \n 3.Libras \n 4.Salir \n ");
 scanf("%d", &opcion);
 printf("ingresa el monto:");
-scanf("%d", &pesos);
+scanf("%f", &pesos);
 //imprimir el resultado
-    switch ( pesos ){
+    switch ( opcion ){
         //declarations
         // . . . 
     case 1:
-    printf( "nada" pesos, (pesos / dolar))
+    printf( "El resultado es %f\n", (pesos / dolar));
     break;
 
     case 2:
-        printf("\n", pesos, (pesos / euro));
+        printf("El resultado es %f\n",(pesos / euro));
         break;
 
     case 3:
-        printf("\n", pesos, (pesos / libra));
+        printf("El resultado es %f\n", (pesos / libra));
         break;
     
     default:
